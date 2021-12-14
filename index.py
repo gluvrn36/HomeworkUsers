13925a1f30124126b07c6180a0a905cf8be909f2
Alexander Glushkov <sasha698@yandex.ru>
2021-12-14 10:06:02 +0300

    Point 2

--- index.py
+++ index.py
@@ -8,3 +8,11 @@ for user in users:
     if user['password'].isdigit():
         users_wrong_password.append({'name': user['name'], 'mail': user['mail']})
 pprint(users_wrong_password)
+
+girls_drivers = []
+
+for friends in user.get('friends'):
+    if friends['sex'] == 'F' and friends.get('cars'):
+        girls_drivers.append(friends['name'])
+
+pprint(girls_drivers)
